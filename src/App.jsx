13332537340ea64d426fe8925dd23bcd19f553e1
Harry <nlizrraga.000@gmail.com>
2025-11-25 20:33:38 -0300

    agrega dependencia para react router dom, corrige errores de import. componente navbar funcional

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,23 @@ import Footer from "./components/Footer";
 import { Route, Routes, BrowserRouter } from 'react-router'
 import Login from './components/views/Login.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import Navbar from './components/views/Navbar/Navbar.jsx';
+import RegistrarPaciente from './components/views/RegistrarPaciente.jsx';
 function App() {
 
   return (
     <>
-
-     <h1>CLINICA</h1>
-    
-
-     <main>
       <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/registrarPaciente'element={<RegistrarPaciente />} />
-      </Routes>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<h1>Bienvenido a la Clínica</h1>} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/registrarPaciente' element={<RegistrarPaciente />} />
+          </Routes>
+        </main>
+        <Footer />
       </BrowserRouter>
-     </main>
-      <Footer />
 
     </>
   )
