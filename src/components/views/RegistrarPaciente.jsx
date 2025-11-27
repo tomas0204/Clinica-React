fd0cb148d030830d01097e8249f48f72a38650a1
Nahuel Nuñez <nunezn53@gmail.com>
2025-11-27 18:01:26 -0300

    copio componente rama miguel

--- src/components/views/RegistrarPaciente.jsx
+++ src/components/views/RegistrarPaciente.jsx
@@ -1,15 +1,25 @@
+import { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import { FaUserMd } from 'react-icons/fa';
+
 
 
 const RegistrarPaciente = () => {
+  
 
 
 
 
-    return (
+  return (
     <>
-      
+       
     </>
-  )
-}
+  );
+};
 
-export default RegistrarPaciente
+export default RegistrarPaciente;
+
+
