cf3c2fdfc865137253560db4fb97c10ef9370207
Tomás <149749382+tomas0204@users.noreply.github.com>
2025-11-28 22:04:50 -0300

    Saqué una url innecesaria de App

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import RegistrarPaciente from './components/views/RegistrarPaciente.jsx'
 import Login from './components/views/Login.jsx'
 import Home from "./components/views/Home.jsx"
 import TurnosList from './components/views/TurnosList.jsx'
-import CrearTurno from '../src/components/turnos/CrearTurno.jsx';
 import Footer from "./components/shared/Footer.jsx";
 import Navbar from './components/shared/Navbar.jsx';
 import './App.css'
@@ -25,7 +24,6 @@ function App() {
         <Route path='/registrarPaciente'element={<RegistrarPaciente />} />
         <Route path='*' element={<Error404 />} />
         <Route path='/turnos' element={<TurnosList />} />
-        <Route path='/turnos/crear' element={<CrearTurno />} />
       </Routes>
       </main>
       <Footer />
