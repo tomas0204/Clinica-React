3647195c86698a36943b0c04446cf7a26b0d7c44
Tomás <149749382+tomas0204@users.noreply.github.com>
2025-11-28 21:51:29 -0300

    Formulario principal -> TurnosList (query nombrePaciente se muestra en pantalla al crear el turno)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes, BrowserRouter } from 'react-router'
 import RegistrarPaciente from './components/views/RegistrarPaciente.jsx'  
 import Login from './components/views/Login.jsx'
 import Home from "./components/views/Home.jsx"
+import TurnosList from './components/views/TurnosList.jsx'
+import CrearTurno from '../src/components/turnos/CrearTurno.jsx';
 import Footer from "./components/shared/Footer.jsx";
 import Navbar from './components/shared/Navbar.jsx';
 import './App.css'
@@ -22,6 +24,8 @@ function App() {
         <Route path='/login' element={<Login/>}/>
         <Route path='/registrarPaciente'element={<RegistrarPaciente />} />
         <Route path='*' element={<Error404 />} />
+        <Route path='/turnos' element={<TurnosList />} />
+        <Route path='/turnos/crear' element={<CrearTurno />} />
       </Routes>
       </main>
       <Footer />
