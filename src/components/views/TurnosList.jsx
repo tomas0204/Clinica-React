7ae0d484d5b8b553e01e0159607186b71ceb9fe4
Tomás <149749382+tomas0204@users.noreply.github.com>
2025-11-29 22:54:57 -0300

    Boton borrar y editar turno, junto con sus funcionalidades

--- src/components/views/TurnosList.jsx
+++ src/components/views/TurnosList.jsx
@@ -1,17 +1,23 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Button } from 'react-bootstrap'
 import CrearTurno from '../turnos/CrearTurno.jsx';
-import Table from 'react-bootstrap/Table';
+import Table from 'react-bootstrap/Table'
+import Swal from 'sweetalert2'
 
 
 const TurnosList = () => {
 
     const [turnos, setTurnos] = useState([])
+    const [mode, setMode] = useState("crear");
+    const [show, setShow] = useState(false);
+    const [turnoEdit, setTurnoEdit] = useState(null);
+
     const pacientes = [
         { id: 1, nombre: "Juan Perez" },
         { id: 2, nombre: "Maria Gomez" },
         { id: 3, nombre: "Carlos Lopez" }
     ];
+
 
     const medicos = [
         { id: 1, nombre: "Dr. Smith" },
@@ -20,17 +26,66 @@ const TurnosList = () => {
     ];
 
 
+    const handleDelete = (id) => {
+    Swal.fire({
+        title: "¿Eliminar turno?",
+        text: "Esta acción no se puede deshacer",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "#3085d6",
+        confirmButtonText: "Sí, eliminar",
+        cancelButtonText: "Cancelar"
+    }).then((result) => {
+        if (result.isConfirmed) {
+
+            setTurnos(turnos.filter(t => t.id !== id));
+
+            Swal.fire({
+                title: "Eliminado",
+                text: "El turno ha sido eliminado",
+                icon: "success",
+                timer: 2000,
+                showConfirmButton: false
+            });
+        }
+    });
+};
+
+
     return (
         <div>
             <h1>Turnos</h1>
             <CrearTurno
+                show={show}
+                onClose={() => setShow(false)}
+                mode={mode}
+                turnoEdit={turnoEdit}
                 onSave={(nuevoTurno) => {
-                    console.log("Turno guardado:", nuevoTurno);
-                    setTurnos([...turnos, nuevoTurno]);
+                    if (mode === "crear") {
+                        const nuevoTurnoConId = { ...nuevoTurno, id: Date.now() };
+                        setTurnos([...turnos, nuevoTurnoConId]);
+                    } else {
+                        setTurnos(turnos.map(t =>
+                            t.id === turnoEdit.id ? nuevoTurno : t
+                        ));
+                    }
                 }}
                 pacientesMock={pacientes}
                 medicosMock={medicos}
             />
+
+            <Button
+                variant="primary"
+                onClick={() => {
+                    setMode("crear");
+                    setTurnoEdit(null);
+                    setShow(true);
+                }}
+            >
+                Nuevo Turno
+                <i className="bi bi-plus-circle me-2 ms-2"></i>
+            </Button>
 
 
             <div className="table-responsive">
@@ -43,6 +98,7 @@ const TurnosList = () => {
                             <th>Hora</th>
                             <th>Motivo de Consulta</th>
                             <th>Estado</th>
+                            <th>Acciones</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -59,6 +115,27 @@ const TurnosList = () => {
                                     <td>{t.hora}</td>
                                     <td>{t.motivoConsulta}</td>
                                     <td>{t.estado}</td>
+                                    <td>
+                                        <Button
+                                            variant="dark"
+                                            className='me-2'
+                                            onClick={() => {
+                                                setMode("editar");
+                                                setTurnoEdit(t);
+                                                setShow(true);
+                                            }}
+                                        >
+                                            <i class="bi bi-pen-fill"></i>
+                                        </Button>
+                                        <Button
+                                            variant="danger"
+                                            onClick={() => (handleDelete(t.id))}
+
+                                        >
+                                            <i class="bi bi-person-x"></i>
+                                        </Button>
+                                    </td>
+
                                 </tr>
                             ))
                         )}
