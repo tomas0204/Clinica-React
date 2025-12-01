3b5c46aee26305be9ebe61c05669c6abe1322cfd
Luana Jakubowicz <luanajakubowicz@hotmail.com>
2025-12-01 07:43:59 -0300

    Estilos básicos al footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,51 +1,52 @@
-import { Container, Row, Col} from 'react-bootstrap';
+import React from "react";
+import { Container, Row, Col } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
-import image from "../../../public/img/Bienestar360.png"
-import "./Footer.css";
+import { Link } from 'react-router-dom';    
+
 
 function Footer() {
     return (
-        <footer className="footer">
-            <Container className= "footer-container">
+        <footer>
+            <Container>
                 <Row>
-                    <Col className="footer-logo">
-                    <img src={image} alt="Logo" />
-                    <p>Salud que te envuelve</p>
+                    <Col>
+                        <h4>Logo de la clínica</h4>
+                        <p>Slogan</p>
                     </Col>
-                
-                <Col className="footer-column">
-                    <Nav className="flex-column footer-nav">
-                    <Nav.Item>
-                        <strong>Pacientes</strong>
-                    </Nav.Item>
-                    <Nav.Link href="#">Turnos</Nav.Link>
-                    <Nav.Link href="#">Mis turnos</Nav.Link>
-                    <Nav.Link href="#">Login</Nav.Link>
-                    <Nav.Link href="#">Registro</Nav.Link>
-                </Nav>
-            </Col>
 
-            <Col className="footer-column">
-                <Nav className="flex-column footer-nav">
-                    <Nav.Item>
-                    <strong>Profesionales</strong>
-                    </Nav.Item>
-                    <Nav.Link href="#">Panel</Nav.Link>
-                    <Nav.Link href="#">Login</Nav.Link>
-                    <Nav.Link href="#">Registro</Nav.Link>
-                </Nav>   
-            </Col>
-            
-            <Col className="footer-column">
-                <Nav className="flex-column footer-nav">
-                    <Nav.Item>
-                    <strong>Sobre la clínica</strong>
-                    </Nav.Item>
-                <Nav.Link href="#">Nosotros</Nav.Link>
-                <Nav.Link href="#">Especialides</Nav.Link>
-                <Nav.Link href="#">Contacto</Nav.Link>
-                </Nav> 
-            </Col>
+                    <Col>
+                        <Nav className="flex-column">
+                            <Nav.Item>
+                                <strong>Pacientes</strong>
+                            </Nav.Item>
+                            <Nav.Link as={Link} to="/turnos">Turnos</Nav.Link>
+                            <Nav.Link as={Link} to="/turnos">Mis turnos</Nav.Link>
+                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                            <Nav.Link as={Link} to="/registrarPaciente">Registro</Nav.Link>
+                        </Nav>
+                    </Col>
+
+                    <Col>
+                        <Nav className="flex-column">
+                            <Nav.Item>
+                                <strong>Profesionales</strong>
+                            </Nav.Item>
+                            <Nav.Link href="/*">Panel</Nav.Link>
+                            <Nav.Link href="/login">Login</Nav.Link>
+                            <Nav.Link href="">Registro</Nav.Link>
+                        </Nav>
+                    </Col>
+
+                    <Col>
+                        <Nav className="flex-column">
+                            <Nav.Item>
+                                <strong>Sobre la clínica</strong>
+                            </Nav.Item>
+                            <Nav.Link href="*">Nosotros</Nav.Link>
+                            <Nav.Link as={Link} to="*">Especialidades</Nav.Link>
+                            <Nav.Link href="*">Contacto</Nav.Link>
+                        </Nav>
+                    </Col>
 
                 </Row>
             </Container>
@@ -54,3 +55,7 @@ function Footer() {
 }
 
 export default Footer;
+
+
+
+
