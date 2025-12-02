e3430e05abee3ba7a2b288f24d5db69cc69a033d
Miguel Angel Zambrano <angel245687@gmail.com>
2025-12-02 01:29:01 -0300

    logica editar registroMedico

--- src/components/views/crudMedico/ItemMedico.jsx
+++ src/components/views/crudMedico/ItemMedico.jsx
@@ -4,16 +4,16 @@ import { FaEye } from 'react-icons/fa';
 import { FaPencilAlt } from 'react-icons/fa';
 import { FaTrash } from 'react-icons/fa';
 
-const ItemMedico = ({medico, index, borrarMedico}) => {
+const ItemMedico = ({medico, index, borrarMedico, modificarMedico}) => {
 
 
 
     return (
-    <div className='d-flex'>
+    <div className='d-flex flex-row'>
       <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcSuEBa1zuFIvWpp5uaXrimbkTCnDJ2lbzz7tQBdZvFabvT4xLC2ikF9y7qvhdoijiWKdXlqQzVThT6sqxlhJBkxvjFpa3e2qksd_FK1TMu_ONvju_o" />
       <Card.Body>
-        <Card.Title> Dr {medico.nombre_y_apellido_medico} </Card.Title>
+        <Card.Title> Dr/a {medico.nombre_y_apellido_medico} </Card.Title>
         <Card.Text className='mt-4'>
           Especialidad: {medico.especialidad}
         </Card.Text>
@@ -22,7 +22,7 @@ const ItemMedico = ({medico, index, borrarMedico}) => {
         </Card.Text>
         <div className='d-flex justify-content-center align-item-center gap-2'>
         <Button variant="primary"><FaEye/></Button>
-        <Button variant="warning"><FaPencilAlt/></Button>
+        <Button variant="warning" onClick={() => modificarMedico(medico.email_medico)} ><FaPencilAlt/></Button>
         <Button variant="danger" onClick={() => borrarMedico(medico.email_medico)} ><FaTrash/></Button>
         </div>
       </Card.Body>
