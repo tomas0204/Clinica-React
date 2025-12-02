094ca1136f5aa34e5ecd320da88d869ca6f5a454
Nahuel Nuñez <nunezn53@gmail.com>
2025-12-02 20:56:10 -0300

    doy formatos restantes a los documentos

--- src/components/views/Guardia/FormGuardia.jsx
+++ src/components/views/Guardia/FormGuardia.jsx
@@ -3,11 +3,14 @@ import { FormText } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import Swal from "sweetalert2";
 
-
-
 const FormGuardia = ({ agregarMedico, editarMedico, medicoEditando }) => {
-  const { register, handleSubmit, formState: { errors } , reset, setValue } = useForm();
-
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    setValue,
+  } = useForm();
 
   useEffect(() => {
     if (medicoEditando !== null) {
@@ -19,75 +22,79 @@ const FormGuardia = ({ agregarMedico, editarMedico, medicoEditando }) => {
     }
   }, [medicoEditando, setValue, reset]);
 
-
   const onSubmit = (data) => {
-
     if (medicoEditando !== null) {
       editarMedico(data);
 
       Swal.fire({
-      icon: "success",
-      title: "Guardia editada",
-      text: "Los datos fueron actualizados correctamente",
-      timer: 2000,
-      showConfirmButton: false
-    });
-
+        icon: "success",
+        title: "Guardia editada",
+        text: "Los datos fueron actualizados correctamente",
+        timer: 2000,
+        showConfirmButton: false,
+      });
     } else {
       agregarMedico(data);
 
       Swal.fire({
-      icon: "success",
-      title: "Guardia agregada",
-      text: "La guardia fue registrada correctamente",
-      timer: 2000,
-      showConfirmButton: false
-    });
-  }
+        icon: "success",
+        title: "Guardia agregada",
+        text: "La guardia fue registrada correctamente",
+        timer: 2000,
+        showConfirmButton: false,
+      });
+    }
 
     reset();
   };
 
   return (
     <div className="lista-div container mt-4 pt-1">
-
-      <h2 className="my-4">{medicoEditando ? "Editar Guardia" : "Agregar Guardia"}</h2>
+      <h2 className="my-4">
+        {medicoEditando ? "Editar Guardia" : "Agregar Guardia"}
+      </h2>
 
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-3 d-flex flex-column align-items-center ">
           <label className="form-label fs-4">Nombre del Médico</label>
           <input
             type="text"
-            className={`form-control w-50 text-center ${medicoEditando ? "input-editando" : ""}`}
+            className={`form-control w-50 text-center ${
+              medicoEditando ? "input-editando" : ""
+            }`}
             placeholder="Ej: Dr Pablo"
-            {...register("nombre", { required: "Este campo no puede quedar vacío.",
+            {...register("nombre", {
+              required: "Este campo no puede quedar vacío.",
               pattern: {
                 value: /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/,
-                message: "Solo se permiten letras"
-              }, 
+                message: "Solo se permiten letras",
+              },
               maxLength: {
                 value: 30,
-                message: "El campo de nombre debe tener como maximo 30 caracteres."
-              }
-             })}
+                message:
+                  "El campo de nombre debe tener como maximo 30 caracteres.",
+              },
+            })}
           />
           <FormText className="text-danger w-50 d-flex  fs-6 ">
             <strong>{errors.nombre?.message}</strong>
           </FormText>
         </div>
 
-        
-
         <div className="mb-4 d-flex flex-column align-items-center">
           <label className="form-label fs-4">Horario de Entrada</label>
           <input
             type="time"
-            className={`form-control w-50 text-center ${medicoEditando ? "input-editando" : ""}`}
+            className={`form-control w-50 text-center ${
+              medicoEditando ? "input-editando" : ""
+            }`}
             placeholder="Ej: 08:00"
-            {...register("entrada", { required:"Este campo no puede quedar vacío"})}
+            {...register("entrada", {
+              required: "Este campo no puede quedar vacío",
+            })}
           />
           <FormText className="text-danger w-50 d-flex  fs-6 ">
-            <strong>{errors.entrada?.message} </strong> 
+            <strong>{errors.entrada?.message} </strong>
           </FormText>
         </div>
 
@@ -95,11 +102,15 @@ const FormGuardia = ({ agregarMedico, editarMedico, medicoEditando }) => {
           <label className="form-label fs-4">Horario de Salida</label>
           <input
             type="time"
-            className={`form-control w-50 text-center ${medicoEditando ? "input-editando" : ""}`}
-            {...register("salida", { required: "Este campo no puede quedar vacío" })}
+            className={`form-control w-50 text-center ${
+              medicoEditando ? "input-editando" : ""
+            }`}
+            {...register("salida", {
+              required: "Este campo no puede quedar vacío",
+            })}
           />
           <FormText className="text-danger w-50 d-flex  fs-6 ">
-            <strong>{errors.salida?.message} </strong> 
+            <strong>{errors.salida?.message} </strong>
           </FormText>
         </div>
 
--- src/components/views/Guardia/ListaDeGuardias.jsx
+++ src/components/views/Guardia/ListaDeGuardias.jsx
@@ -1,37 +1,55 @@
 import { Button } from "react-bootstrap";
 
-const ListaDeGuardias = ({ medicos, borrarMedico, seleccionarMedicoParaEditar, indiceEditando }) => {
+const ListaDeGuardias = ({
+  medicos,
+  borrarMedico,
+  seleccionarMedicoParaEditar,
+  indiceEditando,
+}) => {
   return (
-    <div className="lista-div mt-4 pt-3 d-flex flex-column align-items-center" >
-      <h2 >Médicos de Guardia</h2>
+    <div className="lista-div mt-4 pt-3 d-flex flex-column align-items-center">
+      <h2>Médicos de Guardia</h2>
 
-      {medicos.length === 0 && <p className="my-3 fs-5">No hay médicos/as disponibles.</p>}
+      {medicos.length === 0 && (
+        <p className="my-3 fs-5">No hay médicos/as disponibles.</p>
+      )}
 
       <ul className="list-group my-4 w-50">
-
         {medicos.map((medico, index) => (
-          
-          <li key={index} className="list-group-item d-flex flex-column flex-md-row justify-content-between align-items-md-center ">
-
-            <div className=" d-flex flex-column-sm"> 
-              <span className="ms-1 my-1"><strong>Medico/a:</strong> {medico.nombre}</span>
+          <li
+            key={index}
+            className="list-group-item d-flex flex-column flex-md-row justify-content-between align-items-md-center "
+          >
+            <div className=" d-flex flex-column-sm">
+              <span className="ms-1 my-1">
+                <strong>Medico/a:</strong> {medico.nombre}
+              </span>
             </div>
 
             <div>
-              <span className=" mb-1"><strong>Horario:</strong> {medico.entrada} - {medico.salida}</span>
+              <span className=" mb-1">
+                <strong>Horario:</strong> {medico.entrada} - {medico.salida}
+              </span>
             </div>
 
             <div className="d-flex justify-content-center mt-1">
-              <Button variant="danger" className="btn-sm me-2 "  onClick={() => borrarMedico(index)} disabled={indiceEditando !== null}>
-              Borrar
-            </Button>
+              <Button
+                variant="danger"
+                className="btn-sm me-2 "
+                onClick={() => borrarMedico(index)}
+                disabled={indiceEditando !== null}
+              >
+                Borrar
+              </Button>
 
-            <Button variant="warning" className="btn-sm" onClick={() => seleccionarMedicoParaEditar(index)}>
-              Editar
-            </Button>
+              <Button
+                variant="warning"
+                className="btn-sm"
+                onClick={() => seleccionarMedicoParaEditar(index)}
+              >
+                Editar
+              </Button>
             </div>
-            
-            
           </li>
         ))}
       </ul>
