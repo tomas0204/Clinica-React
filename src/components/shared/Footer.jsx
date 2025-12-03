bda4432a1282cf51f43d6ad696972fc0b08b17c0
Tomás <149749382+tomas0204@users.noreply.github.com>
2025-12-03 01:19:25 -0300

    Footer de luhana

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,51 +1,50 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col} from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
-import { Link } from 'react-router-dom';    
-
+import "./Footer.css";
 
 function Footer() {
     return (
-        <footer>
-            <Container>
+        <footer className="footer">
+            <Container className= "footer-container">
                 <Row>
-                    <Col>
-                        <h4>Logo de la clínica</h4>
-                        <p>Slogan</p>
+                    <Col className="footer-logo">
+                    <img src="" alt="Logo" />
+                    <p>Salud que te envuelve</p>
                     </Col>
+                
+                <Col className="footer-column">
+                    <Nav className="flex-column footer-nav">
+                    <Nav.Item>
+                        <strong>Pacientes</strong>
+                    </Nav.Item>
+                    <Nav.Link href="#">Turnos</Nav.Link>
+                    <Nav.Link href="#">Mis turnos</Nav.Link>
+                    <Nav.Link href="#">Login</Nav.Link>
+                    <Nav.Link href="#">Registro</Nav.Link>
+                </Nav>
+            </Col>
 
-                    <Col>
-                        <Nav className="flex-column">
-                            <Nav.Item>
-                                <strong>Pacientes</strong>
-                            </Nav.Item>
-                            <Nav.Link as={Link} to="/turnos">Turnos</Nav.Link>
-                            <Nav.Link as={Link} to="/turnos">Mis turnos</Nav.Link>
-                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                            <Nav.Link as={Link} to="/registrarPaciente">Registro</Nav.Link>
-                        </Nav>
-                    </Col>
-
-                    <Col>
-                        <Nav className="flex-column">
-                            <Nav.Item>
-                                <strong>Profesionales</strong>
-                            </Nav.Item>
-                            <Nav.Link href="/*">Panel</Nav.Link>
-                            <Nav.Link href="/login">Login</Nav.Link>
-                            <Nav.Link href="">Registro</Nav.Link>
-                        </Nav>
-                    </Col>
-
-                    <Col>
-                        <Nav className="flex-column">
-                            <Nav.Item>
-                                <strong>Sobre la clínica</strong>
-                            </Nav.Item>
-                            <Nav.Link href="*">Nosotros</Nav.Link>
-                            <Nav.Link as={Link} to="*">Especialides</Nav.Link>
-                            <Nav.Link href="*">Contacto</Nav.Link>
-                        </Nav>
-                    </Col>
+            <Col className="footer-column">
+                <Nav className="flex-column footer-nav">
+                    <Nav.Item>
+                    <strong>Profesionales</strong>
+                    </Nav.Item>
+                    <Nav.Link href="#">Panel</Nav.Link>
+                    <Nav.Link href="#">Login</Nav.Link>
+                    <Nav.Link href="#">Registro</Nav.Link>
+                </Nav>   
+            </Col>
+            
+            <Col className="footer-column">
+                <Nav className="flex-column footer-nav">
+                    <Nav.Item>
+                    <strong>Sobre la clínica</strong>
+                    </Nav.Item>
+                <Nav.Link href="#">Nosotros</Nav.Link>
+                <Nav.Link href="#">Especialides</Nav.Link>
+                <Nav.Link href="#">Contacto</Nav.Link>
+                </Nav> 
+            </Col>
 
                 </Row>
             </Container>
@@ -54,5 +53,3 @@ function Footer() {
 }
 
 export default Footer;
-
-
