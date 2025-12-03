446c2098713e4db3c14d4beadc3db070bb4c088c
Tomás <149749382+tomas0204@users.noreply.github.com>
2025-12-03 01:52:20 -0300

    Route de registrarpaciente agregada

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import GuardiaMedica from './components/views/Guardia/GuardiaMedica.jsx'
 import HistoriaClinica from './components/historiaClinica/HistoriaClinica.jsx'
 import ItemMedico from './components/views/crudMedico/ItemMedico.jsx'
 import Error404 from './components/views/Error404.jsx'
+import ItemPacientes from './components/views/Pacientes/ItemPacientes.jsx'
 
 
 
@@ -39,9 +40,7 @@ function App() {
             <Route path='' element={<Home />} />
             <Route path='/' element={<Home />} />
             <Route path='/registrarPaciente' element={<RegistrarPaciente />} />
-
             <Route path='/guardia-medica' element={currentUser?.role === "medico" || currentUser?.role === "admin" ? <GuardiaMedica /> : <Navigate to="/login" />} />
-
             <Route path='/turnos' element={<TurnosList />} />
             <Route path='/login' element={<Login onLogin={setIsAdmin} />} />
             <Route path='/historiaClinica' element={currentUser?.role === "medico" ? <HistoriaClinica /> : <Navigate to="/login" />} />
